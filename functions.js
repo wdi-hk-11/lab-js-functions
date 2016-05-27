c33295d24e89f7ce7e6a8a9f17cf534fcbe369a5
Joyce Cheung <joice@Joyces-MacBook-Pro.local>
2016-05-27 10:29:48 +0800

    finished

--- functions.js
+++ functions.js
@@ -1,44 +1,90 @@
 // Question 1
 function maxOfTwoNumbers() {
 
+function bigger(a,b) { if (a > b ) {return a;} else { return b;}}
+undefined
+bigger(4,7)
+7
 }
 
 // Question 2
 function maxOfThree() {
 
+biggest(c,bigger(a,b))
+
+bigger(2,bigger(4,7))
 }
 
 // Question 3
 function isCharacterAVowel() {
 
+function checkvowel(input) { if (input.includes("o")) { return true;} else { return false; } }
+
 }
 
 // Question 4
 function sumArray() {
- 
+
+ function sum (sumarray) {  var total = 0 ;
+for( var i = 0; i < sumarray.length ; i++ ) {
+
+  total = total + sumarray[i] }
+return total;
+}
 }
 
 
 // Question 4
 function multiplyArray() {
+function sum (sumarray) {  var total = 0 ;
+for( var i = 0; i < sumarray.length ; i++ ) {
 
+  total = total * sumarray[i] }
+return total;
+}
 }
 
 
 // Question 5
 var reverseString = function (){
-  
+
+  var newname = []
+  function reverse(input) { for(var i = 0; i < input.length ; i++) { newname.push((input.charAt(i)))} return console.log(newname.reverse())   }
+
 };
 
 
 // Question 8
 function findLongestWord () {
-  
+
+  function longest(pets){ var longest = "" ; var length = 0 ;for (var i = 0; i < pet.length ; i++ ){
+
+if (pet[i].length > length ) {
+    var length = pet[i].length;
+    longest = pet[i];
+}
+}
+
+return longest;
+}
 }
 
 // Question 9
 function filterLongWords () {
-  
+
+    function longerthan(pets, num){ var longest = "" ; var newlist = [] ;var length = 0 ;for (var i = 0; i < pet.length ; i++ ){
+
+if (pet[i].length > num ) {
+    var length = pet[i].length;
+    longest = pet[i];
+    newlist.push(pet[i])
+}
+}
+
+return newlist;
+
+}
+
 }
 
 
@@ -48,10 +94,10 @@ function filterLongWords () {
 
 // Bonus 2
 function charactersOccurencesCount() {
-  
+
 }
 
 // Bonus 3
 var numberOfArguments = function(){
-  
+
 }
